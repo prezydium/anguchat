71d0b730480e027fdc1c55bda34b55e30bf005d5
Krzysztof Syska <prezydium834@gmail.com>
2018-08-26 11:04:43 +0200

    Initial commit

---
+++ src/main/java/org/prezydium/anguchat/AnguchatApplication.java
@@ -0,0 +1,12 @@
+package org.prezydium.anguchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnguchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnguchatApplication.class, args);
+	}
+}
