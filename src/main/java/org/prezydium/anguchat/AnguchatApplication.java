f832124824eb5aa60597fe2eb8a43fa26753f94e
krzysztof.syska <prezydium834@gmail.com>
2018-08-26 20:09:55 +0200

    basic styling

--- src/main/java/org/prezydium/anguchat/AnguchatApplication.java
+++ src/main/java/org/prezydium/anguchat/AnguchatApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AnguchatApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AnguchatApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AnguchatApplication.class, args);
+    }
 }
--- src/main/java/org/prezydium/anguchat/ChatRoomController.java
+++ src/main/java/org/prezydium/anguchat/ChatRoomController.java
@@ -1,20 +1,15 @@
 package org.prezydium.anguchat;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.util.Map;
 
 @RestController
 public class ChatRoomController {
